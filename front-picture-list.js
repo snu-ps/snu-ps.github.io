73c8dea5e60753c4d98076c6f5c445d87d3c8da5
ntopia <ntopia.p@gmail.com>
2017-10-02 00:06:18 +0900

    사진을 github에 저장하지 않고 Google Photo 에서 불러오도록 함

---
+++ front-picture-list.js
@@ -0,0 +1,39 @@
+var albums = [
+    {
+        'title': '2017 여름 MT',
+        'pictures': [
+            'https://lh3.googleusercontent.com/wKp4hwtdF1OeTmjzY89Hvwh582JmBprFwXaFzah-XwTkmfbGSOuRBPVemFVRKQpjlraxvtlNl9vV10bPJA=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/JLV2sfdfLJdJ6wWLV2YVrzWpen4094R8As-OIgK5rEnD55dSZIAG0wXGRZq9quihtFtc6LYmXFleqAVhFg=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/S5-1MxCF36M4uYw0hUwiinK9fc_B08XtGR9zXcexYQ-eBa5FeyGIX2u9W99p5K4hBk-9fpzX6jBqigQEIQ=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/O_-nkf8geptyxdLREQRVlk59bS8GwqzJDFhKDE9pCBXlNedZ2lTpvpyKuz23DrmtMQ12ntXW_W2d8Pyb=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/kyYoCHTid5CoxjDmVANl0-7tvTObHYCvwOiDwn7AqUVPpLCwOIA78RmGWeb4PT3fXyZh5rky8V_xP0aJ=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/CxNMGxJODS6_mfzhBH9qUtn6B-yVRsjUvnBmaxkpgmjbaBhs4V2tmrbI70gI3lZdkp63MKU6NrEbHSDj=w1024-no-tmp.jpg',
+        ]
+    },
+    {
+        'title': '2017 ACM-ICPC 대전 리저널 인터넷 예선',
+        'pictures': [
+            'https://lh3.googleusercontent.com/zQtBNjan52oCcdqEZqb6hy5YFlH7HeVsegy2iHYbPNKPjnAW3yS0wJnx_MDlgtvAPBnH5sSztpQ-3PRZ3g=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/OkFMauOzCq8590tZbTtabuHIMKAH9iihAA_aCvsvHePxwn74Dzv9fRyHR46HqmW7sYthQhCy5CGufJRR=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/zXltyDNbs0vTMMi4VOLCUY5SULmOf2gDgrMVRFJ-t8czJk8udfPWzcMEWrPUuB5LQG10JxCMXvjWzv31=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/GCyWsBeMq8EOnlAdGHGCpbTJNlnqnXXVW94ZCDj3znokxfMWemCHkzUAD_juzAvNAIEbpGqv5XayxA-w=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/STQzbSZfLBRrbIeYnl_fk8ztO-eoe5H6rEFtfJX44mVw9uPzFv4Ks05qKB1XzgYQmWfCuRcMGWcDFsx-=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/n4olMY0VK_2BsOf1R1pohlhvFU78qVyGCGiHJjFXqtFXmBSzen9MfZZYcZq-2_m6lzpajA_2YblKDe5R=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/7niZ9j-HgQOSnja7tJSFIiJrIaGazYCeaz2WwErF4tQ8AkaspLUVDaD4eooKx7OTgdU84cgaIGdI5_AM=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/9GSnDGqTSd7OrQ1CsGS6wPp4ztQ9RENYZAfu0ZPx1qsFsHV60LHmzG4MZ_sS5Pr5gN4NkcTs4R9zRfo-=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/elrSVrZFKI-4nrOt2Ty-xvMzvLDnjfHTRORq7iDDrUPmik5O8E9CtSo2IzsVOH64qxvqpZIQjZyox-9n=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/G9YkZAWFX1SnJr1FgU5ErTz1DULSl29jZ6sHaXaZB8mxwYtx_L09oJuhJRKPW_cqJDEjfnoMBiD0kGUG=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/7KPlmXlNiK988fCTfzFsTJZvAzhb1uCfmsCV6CX42Np5Ya_Sv-uJigZ7zqvjxKKPJcFNaGWSz7I_PUzD=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/RUpEdH7Cbqy-LmAOzp5T5Ltx7UFfk-WyzxozUXIzir_niX2evg4h1KBHM8-uGb9QK380MOEzVQUDpQrx=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/QW8Kd9fUnJABpbI7voky4Yd635ad2AfuLfy6DQLQHMMVIEvI7bMxskzjvh4RCzcSPhUXUPf2jcmYlynQ=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/o_cRBK3nwRSXYEEIPjSkSHF1Oqm9IIeeXVJgULY273cl7VgJAccUeQUHrSXD-Ztbt17roCUvGOOdsUNI=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/ixUUKC7VsJlgXcDigHeJFeuMJX-iKXVLFNZ330Ouz6FLx-bJ00DND6o_PqgGPxAUPt-6_nGtMnIf-c2w=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/_lqYH77S8Yu7huHga6G3NAcgSdg6rzTNVMWKEzfrmRWBmAwhZ446UALNPwgo1BGXitYrNPRezKW-J6dO=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/C6Lv7WyXxq_sMK0CV_wyAFIb8MY7vdkWdhtoaAP4hCUR1yFecoOzhpASz5-L-pRRkDoEzLpNVLUzxbP_=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/By2H_t4uMiSQOQcg0nel8L5rsNlkJ7_ROs8ppVX9QOpKip8qs-ZpeZaCExslb_4d_DsY_KOrt0NmCD_-=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/OtPZUysb7Xl9GuXhHUvCv4bMva6fYTftuyXr1d8Vm8zlJFHW58Agoug2BX7sE3wqN694o5rsaovI9T0i=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/0XA-VM-j_uoic415k9sFxyWqU8P1bsWlESonh8kLMzGiITD71vw-19wqs8hPeM9nyEUyoq2lJ1Q9hkPO=w1024-no-tmp.jpg',
+            'https://lh3.googleusercontent.com/OOBCVexoy9r25EBUysBZnGzfMBKtazr5g5wx_LcsopetiatWiQme4T6t_Ar0MG4O1LYKj5Nz91wG43vO=w1024-no-tmp.jpg',
+        ]
+    },
+];
